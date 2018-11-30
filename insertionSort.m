2f94bfae363e6995a899e44d1b812f12dda173cf
markonovic00 <markonovic00@gmail.com>
2018-11-30 10:16:41 +0100

    Insertion sort

--- insertionSort.m
+++ insertionSort.m
@@ -5,7 +5,7 @@ function A= insertionSort(A)
    for j=2:length(A)
      key=A(j);
      i=j-1;
-     while(i>0 && A(i)>key)
+     while(i>0 && A(i)>key) % A(i)<key je opadajuce
         A(i+1)=A(i);
         i=i-1;
      endwhile

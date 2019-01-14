abbe2bc7931814776f4f180f07facb9fe865a2d1
markonovic00 <markonovic00@gmail.com>
2019-01-14 11:34:44 +0100

    IgorMihajlo

---
+++ PaintGraph/DFS_Paint.m
@@ -0,0 +1,70 @@
+% DFS
+% 'O' 'T' 'M' 'P'
+
+function G=DFS_Paint(G)
+  
+  global colors;
+  colors=['O' 'T' 'M' 'P'];
+  v=1:length(G.AdjMatrix);
+  
+  for u=v
+    
+    G.V(u).col='W';
+    G.V(u).color=colors(1);
+    G.V(u).pred=NaN;
+    
+  endfor
+  
+  
+  for u=v
+    
+    if G.V(u).col=='W'
+      
+      G = DFS_Paint_Visit(G,u);
+      
+    endif
+    
+  endfor
+  
+endfunction
+
+
+function G = DFS_Paint_Visit(G,u)
+  
+  count=0;
+  global colors;
+  G.V(u).col='G';
+  cbr=0;
+  for v=find(G.AdjMatrix(u,:))
+    
+    if G.V(v).col=='W'
+      
+      G.V(v).pred=u;
+      count++;
+      
+      if G.V(u).color~=G.V(v).color && count > 2
+        
+        G=DFS_Paint_Visit(G,v);
+        
+      endif
+      while G.V(u).color==G.V(v).color
+        
+        cbr++;
+        if cbr==5
+          
+          disp("Nemoguce");
+          return;
+          
+        endif
+        
+        G.V(v).color=colors(cbr);
+        
+      endwhile
+      
+    endif
+    
+  endfor
+  
+  G.V(u).col='W';
+  
+endfunction
---
+++ PaintGraph/skripta.m
@@ -0,0 +1,28 @@
+% Zadatak: Kreirati skriptu i funkciju ObojiGraf() koja treba da oboji graf u 4 boje,
+% tako da svaki čvor bude obojen sa tačno jednom od 4 boje, a nijedna dva susedna
+% čvora u grafu ne smeju biti obojena istom bojom.
+%     Skripta je dužna da kreira graf sa bar 10 čvorova, pozove funkciju ObojiGraf() i
+% prikaže tako dobijeni graf. Ukoliko to nije moguće, ispisati odgovarajuću poruku.
+%     Funkcija ObojiGraf treba da bude realizovana kao modifikacija DFS algoritma,
+% pri čemu se svakom čvoru dodeli neka od 4 boje, a ukoliko sa tom bojom nije
+% moguće rešiti problem, promeniti boju, itd... Ukoliko nije moguće dati čvor obojiti
+% nijednom od 4 boje, vratiti vrednost false.
+
+clc;
+clear;
+
+G.AdjMatrix=[
+0 1 1 1 0 0 0 0 0 0;
+1 0 0 1 1 0 0 0 0 0;
+1 0 0 1 0 1 0 0 0 0;
+1 1 1 0 0 0 1 0 0 0;
+0 1 0 0 0 0 1 1 0 0;
+0 0 1 0 0 0 1 1 0 0;
+0 0 0 1 1 1 0 1 0 0;
+0 0 0 0 1 1 1 0 1 0;
+0 0 0 0 0 0 0 1 0 1;
+0 0 0 0 0 0 0 0 1 0;
+];
+
+Gp=DFS_Paint(G);
+plotGraph(Gp);
